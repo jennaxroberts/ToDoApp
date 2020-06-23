0523f43d00124b1d6fbf63120c1a0c8c81558e08
jenna <jennarob916@gmail.com>
2020-06-23 10:47:09 -0400

    initial commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Jenna Roberts on 6/22/20.
+//  Copyright © 2020 Jenna Roberts. All rights reserved.
+//
+
+import Foundation
+
+
+//Create a ToDo class that has 2 properties… name and important - if you need a refresher on how to create a class and add properties, take a look back at this lesson
+
+class toDo {
+    var name = ""
+    var important = false
+    
+}
